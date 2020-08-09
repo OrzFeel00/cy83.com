dbe95a7be2603e941f697b59775cfbf82f19bf34
OrzFeel00 <632360579@qq.com>
2020-08-09 10:12:55 +0800

    2完成

--- aspring/src/test/java/com/yc/spring/HelloTest.java
+++ aspring/src/test/java/com/yc/spring/HelloTest.java
@@ -4,6 +4,8 @@ package com.yc.spring;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.yc.spring.dao.UserDao;
+
 
 public class HelloTest {
 	 @Test
@@ -18,5 +20,22 @@ public class HelloTest {
 		h.sayHello();
 		ctx.close();
 	}
+	 
+	 
+	 @Test
+		public void test2() {
+			
+			ClassPathXmlApplicationContext ctx=
+					new ClassPathXmlApplicationContext("beans.xml");
+			
+			UserDao udao=(UserDao) ctx.getBean("mdao");
+			UserDao udao1=(UserDao) ctx.getBean("odao");
+			
+			udao.selectUserId("zhangs");
+			udao1.selectUserId("zhangs");
+			
+		
+			ctx.close();
+		}
 
 }
